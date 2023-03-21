1d6d91e5b2132b5900e41adb38625e1622157d82
stephane.chevrier <stephane.chevrier@le-campus-numerique.fr>
2023-03-21 09:53:35 +0100

    1er commit

---
+++ src/main/java/campus/tictactoe4/TicTacToe4Application.java
@@ -0,0 +1,13 @@
+package campus.tictactoe4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToe4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicTacToe4Application.class, args);
+    }
+
+}
---
+++ src/test/java/campus/tictactoe4/TicTacToe4ApplicationTests.java
@@ -0,0 +1,13 @@
+package campus.tictactoe4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToe4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
